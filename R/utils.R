62889f9551fba5f3cfdfb8110734321c12b3d5bb
jcoenep <jcoenep@gmail.com>
2016-03-01 12:21:11 +0800

    timeNodes to utils added for dynamise

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 # startUpMessage -------------------------------
 .onAttach <- function(libname, pkgname = "fbAdsInsightsR") {
-  packageStartupMessage("See ?getEdges and ?getNodes for examples")
+  packageStartupMessage("Run package?graphTweets for examples")
 }
 
 # clean handles
@@ -19,4 +19,37 @@ cleanHandles <- function(handles) {
   }
   
   return(handles)
+}
+
+# timeNodes
+timeNodes <- function(data){
+  
+  # split
+  src <- data[, c("source", "start.stamp", "end.stamp")]
+  tgt <- data[, c("target", "start.stamp", "end.stamp")]
+  
+  # bind
+  names(tgt)[1] <- "source"
+  nodes <- rbind.data.frame(src, tgt)
+  
+  # get earliest time.stamp
+  n_grp <- dplyr::group_by(nodes, source)
+  n_s <- dplyr::slice(n_grp, which.min(start.stamp))
+  n_s <- as.data.frame(n_s)
+  
+  # get latest
+  n_e <- dplyr::slice(n_grp, which.max(end.stamp))
+  n_e <- as.data.frame(n_e)
+  
+  # remove unwanted columns before join
+  n_s$end.stamp <- NULL
+  n_e$start.stamp <- NULL
+  
+  # merge
+  nodes <- dplyr::inner_join(n_s, n_e, by = "source")
+  
+  names(nodes)[1] <- c("label")
+  
+  return(nodes)
+  
 }
