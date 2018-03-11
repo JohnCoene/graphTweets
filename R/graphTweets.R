1d4ccc681ba4f3d9679ac4ab53161c111b10b998
John Coene <jcoenep@gmail.com>
2018-03-11 15:37:48 +0100

    pkg doc

--- R/graphTweets.R
+++ R/graphTweets.R
@@ -1,5 +1,17 @@
 #' \pkg{graphTweets} visualise Twitter Interactions.
+#'  
+#' @section v4:
+#' \itemize{
+#' \item \code{\link{gt_edges}} \code{\link{gt_edges_}} - get edges from tweets
+#' \item \code{\link{gt_nodes}} - add meta-data to vertices
+#' \item \code{\link{gt_dyn}} - create dynamic graphs
+#' \item \code{\link{gt_collect}} - collect edges and nodes
+#' \item \code{\link{gt_graph}} - create graph
+#' \item \code{\link{gt_save}} - save graph to file
+#' }
 #' 
+#'  
+#' @section v3.2:
 #' \itemize{
 #' \item \code{\link{getEdges}} - get edges from tweets
 #' \item \code{\link{getNodes}} - add meta-data to vertices
@@ -10,6 +22,31 @@
 #' 
 #' @examples 
 #' \dontrun{
+#' ## v4
+#' library(rtweet)
+#' 
+#' # Sys.setlocale("LC_TIME", "English")
+#' 
+#' tweets <- search_tweets("#rstats")
+#' 
+#' library(graphTweets)
+#' 
+#' # simple network
+#' tweets %>% 
+#'   gt_edges(text, screen_name) %>% # get edges
+#'   gt_nodes %>% # get nodes
+#'   gt_graph %>% # build igraph object
+#'   plot(.)
+#'   
+#' # dynamic graph
+#' tweets %>% 
+#'   gt_edges(text, screen_name, "created_at") %>% # add created time
+#'   gt_nodes(TRUE) %>% 
+#'   gt_graph %>% 
+#'   gt_dyn %>% # make dynamic
+#'   gt_save # save as .graphml
+#' 
+#' ## v3.2
 #' # authenticate
 #' token <- twitteR::setup_twitter_oauth(consumer_key, consumer_secret, 
 #'                                       access_token, access_secret)
